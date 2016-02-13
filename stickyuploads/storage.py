a803bd16596ba94a5888f2bde39923c73c181b82
Martin Ivanov <martin.iv96@gmail.com>
2016-02-13 22:46:33 +0200

    Make a way to recognize project tmp folders

--- stickyuploads/storage.py
+++ stickyuploads/storage.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 
 from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 
 
 class TempFileSystemStorage(FileSystemStorage):
@@ -11,13 +12,19 @@ class TempFileSystemStorage(FileSystemStorage):
         super(TempFileSystemStorage, self).__init__(
             location=tempfile.gettempdir(), base_url=None)
 
+    def get_prefix(self):
+        """Return prefix for easy recognition of project tmp folders"""
+        if settings.FILE_UPLOAD_TEMP_DIR:
+            return settings.FILE_UPLOAD_TEMP_DIR
+        return ''
+
     def url(self, name):
         """Files are not accessable via url."""
         raise NotImplementedError()
 
     def get_available_name(self, name, max_length=None):
         """Return relative path to name placed in random directory"""
-        tempdir = tempfile.mkdtemp(dir=self.base_location)
+        tempdir = tempfile.mkdtemp(prefix=self.get_prefix(), dir=self.base_location)
         name = os.path.join(
             os.path.basename(tempdir),
             os.path.basename(name),
